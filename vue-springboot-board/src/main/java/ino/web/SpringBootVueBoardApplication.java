103c3e21688254e40f797253df5198df1922426c
seo0tak <seo0tak@gmail.com>
2020-03-03 13:05:59 +0900

    update spring-vue-board

---
+++ vue-springboot-board/src/main/java/ino/web/SpringBootVueBoardApplication.java
@@ -0,0 +1,13 @@
+package ino.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootVueBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootVueBoardApplication.class, args);
+	}
+
+}
---
+++ vue-springboot-board/src/test/java/ino/web/SpringBootVueBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package ino.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootVueBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
